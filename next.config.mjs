58d0c3b9efc5afdb8726040110d988a3fed7547a
hyeyoom <neoul_chw@icloud.com>
2025-03-15 00:42:09 +0900

    feat: Next.js 설정에 캐시 제어 헤더 추가
    - 모든 경로에 대해 1년 동안 캐시되는 헤더 추가
    - API 경로에 대해 1초 동안 캐시되는 헤더 설정
    - 캐시 제어를 통해 성능 최적화 및 사용자 경험 개선

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=31536000, stale-while-revalidate=59',
+          },
+        ],
+      },
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 's-maxage=1, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
